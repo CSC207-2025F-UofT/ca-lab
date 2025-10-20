1ff1f86eccdd9d3ea19d0699e2dc15aa373c4195
Julia Znaczko <lanatkachuk@Lanas-MacBook-Pro-46.local>
2025-10-20 09:59:00 -0400

    Tried our best <3

--- src/main/java/interface_adapter/logout/LogoutPresenter.java
+++ src/main/java/interface_adapter/logout/LogoutPresenter.java
@@ -2,6 +2,7 @@ package interface_adapter.logout;
 
 import interface_adapter.ViewManagerModel;
 import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginState;
 import interface_adapter.login.LoginViewModel;
 import use_case.logout.LogoutOutputBoundary;
 import use_case.logout.LogoutOutputData;
@@ -18,7 +19,9 @@ public class LogoutPresenter implements LogoutOutputBoundary {
     public LogoutPresenter(ViewManagerModel viewManagerModel,
                           LoggedInViewModel loggedInViewModel,
                            LoginViewModel loginViewModel) {
-        // TODO: assign to the three instance variables.
+        this.loggedInViewModel = loggedInViewModel;
+        this.viewManagerModel = viewManagerModel;
+        this.loginViewModel = loginViewModel;
     }
 
     @Override
@@ -29,16 +32,19 @@ public class LogoutPresenter implements LogoutOutputBoundary {
         // We also need to set the username in the LoggedInState to
         // the empty string.
 
-        // TODO: have prepareSuccessView update the LoggedInState
         // 1. get the LoggedInState out of the appropriate View Model,
+        final LoginState loginState = loginViewModel.getState();
         // 2. set the username in the state to the empty string
+        loginState.setUsername("");
         // 3. firePropertyChanged so that the View that is listening is updated.
+        loginViewModel.firePropertyChange();
 
-        // TODO: have prepareSuccessView update the LoginState
         // 1. get the LoginState out of the appropriate View Model,
+        viewManagerModel.setState(loginViewModel.getViewName());
         // 2. set the username in the state to be the username of the user that just logged out,
+        loginState.setUsername(response.getUsername());
         // 3. firePropertyChanged so that the View that is listening is updated.
-
+        viewManagerModel.firePropertyChange();
         // This code tells the View Manager to switch to the LoginView.
         this.viewManagerModel.setState(loginViewModel.getViewName());
         this.viewManagerModel.firePropertyChange();
