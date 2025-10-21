08539949eabf86780ca3b62c087e958bf6e4ef0a
amyjq <amyjqian@gmail.com>
2025-10-21 11:45:35 -0400

    second todo

--- src/main/java/interface_adapter/logout/LogoutPresenter.java
+++ src/main/java/interface_adapter/logout/LogoutPresenter.java
@@ -2,6 +2,7 @@ package interface_adapter.logout;
 
 import interface_adapter.ViewManagerModel;
 import interface_adapter.logged_in.LoggedInViewModel;
+import interface_adapter.login.LoginState;
 import interface_adapter.login.LoginViewModel;
 import use_case.logout.LogoutOutputBoundary;
 import use_case.logout.LogoutOutputData;
@@ -34,8 +35,12 @@ public class LogoutPresenter implements LogoutOutputBoundary {
 
         // TODO: have prepareSuccessView update the LoggedInState
         // 1. get the LoggedInState out of the appropriate View Model,
+        final LoginState loggedInState = loginViewModel.getState();
         // 2. set the username in the state to the empty string
+        loggedInState.setUsername("");
         // 3. firePropertyChanged so that the View that is listening is updated.
+        loginViewModel.firePropertyChange();
+
 
         // TODO: have prepareSuccessView update the LoginState
         // 1. get the LoginState out of the appropriate View Model,
